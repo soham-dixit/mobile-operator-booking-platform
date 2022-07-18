f47983294765f501d322bdcba774c0c13b726430
sohamdixit <sohamddixit@gmail.com>
2022-07-18 16:20:31 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mobile-operator-booking-platform
+SIH2022 Problem Statement - PK836 | Team CODE APOCALYPSE
---
+++ aapka_aadhaar/android/app/src/main/kotlin/com/example/aapka_aadhaar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aapka_aadhaar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
