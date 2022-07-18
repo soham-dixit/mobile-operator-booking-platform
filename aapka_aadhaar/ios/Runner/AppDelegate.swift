f6455c48d17cb71e0259265eb7327efed489263e
sohamdixit <sohamddixit@gmail.com>
2022-07-18 18:35:14 +0530

    google maps setup

--- aapka_aadhaar/ios/Runner/AppDelegate.swift
+++ aapka_aadhaar/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDkK07H_RBRxPdHLGFf80GzawgrU5fgjBI")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
