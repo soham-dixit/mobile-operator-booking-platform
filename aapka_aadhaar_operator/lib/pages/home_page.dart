7ac39a1fd774256b8359a3bc52faa5238e529977
sohamdixit <sohamddixit@gmail.com>
2022-07-22 16:07:05 +0530

    Operator side navigation drawer

--- aapka_aadhaar_operator/lib/pages/home_page.dart
+++ aapka_aadhaar_operator/lib/pages/home_page.dart
@@ -12,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // drawer: NavigationDrawer(),
+      drawer: NavigationDrawer(),
       appBar: AppBar(
         backgroundColor: Color(0xFFF23F44),
         foregroundColor: Color(0xFFFFFFFF),
