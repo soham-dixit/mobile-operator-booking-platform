3a9fcaea11612b5fac23cd37c24c516914fbdcc2
Kush Agarwal <agarwalkushajay@gmail.com>
2022-07-30 17:26:05 +0530

    Added Contact us info

--- aapka_aadhaar/lib/pages/contact_page.dart
+++ aapka_aadhaar/lib/pages/contact_page.dart
@@ -13,6 +13,7 @@ class _ContactPageState extends State<ContactPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFBF9F6),
       appBar: AppBar(
         title: Text('Contact Us'),
         backgroundColor: Color(0xFFF23F44),
@@ -21,7 +22,41 @@ class _ContactPageState extends State<ContactPage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(),
+          Container(
+            padding: EdgeInsets.all(28),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.phone
+                  ),
+                  title: Text('Toll Free: 1947'),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.link
+                  ),
+                  title: Text('https://uidai.gov.in/contact-support/have-any-questions.html'),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.mail_outline_rounded
+                  ),
+                  title: Text('help@uidai.gov.in'),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.location_on
+                  ),
+                  title: Text('Government of India Bangla Sahib Rd, Behind Kali Mandir, Gole Market, New Delhi - 110001'),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
