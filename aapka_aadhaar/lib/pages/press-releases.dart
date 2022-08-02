731f121b9827a5cdc740a95b086f6e55f74acb18
Sneha Dewani <snehadewani07@gmail.com>
2022-08-02 15:16:47 +0530

    redirected to the press-releases

--- aapka_aadhaar/lib/pages/press-releases.dart
+++ aapka_aadhaar/lib/pages/press-releases.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class PressReleases extends StatefulWidget {
   const PressReleases({Key? key}) : super(key: key);
@@ -30,9 +32,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('UIDAI organizes Workshop on ‘Recent Initiatives for Simplifying Aadhaar usage'),
                   subtitle: Text('1 Jun 2022'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap:release1,
                 ),
               ),
               Card(
@@ -45,9 +45,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('Clarification on Aadhaar sharing issue by UIDAI'),
                   subtitle: Text('29 May 2022'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap: release2,
                 ),
               ),
               Card(
@@ -60,9 +58,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('Multiple Ways to establish Veracity of Aadhaar : UIDAI'),
                   subtitle: Text('4 May 2022'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap: release3,
                 ),
               ),
               Card(
@@ -75,9 +71,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('UIDAI organizes Workshop on ‘Recent Initiatives for Simplifying Aadhaar usage’'),
                   subtitle: Text('22 Apr 2022'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap: release4,
                 ),
               ),
               Card(
@@ -90,9 +84,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('Digital India Scheme and Aadhaar'),
                   subtitle: Text('30 Mar 2022'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap: release5,
                 ),
               ),
               Card(
@@ -105,9 +97,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('Inauguration of Gonda, Varanasi, Saharanpur and Moradabad in UP By Minister of State Rajeev Chandrasekhar'),
                   subtitle: Text('21 Dec 2021'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap: release6,
                 ),
               ),
               Card(
@@ -120,9 +110,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('Inauguration of Gonda, Varanasi, Saharanpur and Moradabad in UP'),
                   subtitle: Text('20 Dec 2021'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap:release7,
                 ),
               ),
               Card(
@@ -135,9 +123,7 @@ class _PressReleasesState extends State<PressReleases> {
                   title: Text('‘Aadhaar 2.0- Ushering the Next Era of Digital Identity and Smart Governance’ workshop from 23rd to 25th November, 2021'),
                   subtitle: Text('25 Nov 2021'),
                   tileColor: Colors.white,
-                  onTap: (() {
-                    
-                  }),
+                  onTap: release8,
                 ),
               ),
             ],
@@ -146,4 +132,85 @@ class _PressReleasesState extends State<PressReleases> {
       ),
     );
   }
+
+  release1() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/UIDAI_organizes_Workshop_on_Recent_Initiatives_for_Simplifying_Aadhaar_usage_22_06_22.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release2() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/Clarification_on_Aadhaar_sharing_issue_by_UIDAI_29_05_22.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release3() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/Multiple_Ways_to_establish_Veracity_of_Aadhaar_UIDAI_04_05_22.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release4() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/UIDAI_organizes_Workshop_on_Recent_Initiatives_for_Simplifying_Aadhaar_usage_22_04_22.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release5() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/Digital_India_Scheme_and_Aadhaar_30_03_22.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release6() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/Press_Release_English_2.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release7() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/Press_Release_English_1.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  release8() async {
+    var url = Uri.parse(
+        "https://uidai.gov.in/images/pressrelease/Press_Release_English_3_Nov_2021.pdf");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
 }
