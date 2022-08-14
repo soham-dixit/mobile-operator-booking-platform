70f12dfaed1c94fcdc2c6ab0bba34727146dc220
devanshmehtaGitHub <devanshgithub@gmail.com>
2022-08-14 13:57:47 +0530

    operator slots

--- aapka_aadhaar_operator/lib/pages/home_page.dart
+++ aapka_aadhaar_operator/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:aapka_aadhaar_operator/pages/navigation_drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
@@ -20,6 +21,28 @@ class _HomePageState extends State<HomePage> {
   final _monthFormatter = DateFormat('MMM');
   List dates = [];
   Location currentLocation = Location();
+  String? dayG;
+  List status = [];
+  List timings = [
+    '10AM - 11AM',
+    '11AM - 12PM',
+    '12PM - 1PM ',
+    '1PM - 2PM',
+    '2PM - 3PM',
+    '3PM - 4PM',
+    '4PM - 5PM',
+    '5PM - 6PM'
+  ];
+  List slot = [
+    '10_11',
+    '11_12',
+    '12_1',
+    '2_3',
+    '3_4',
+    '4_5',
+    '5_6',
+  ];
+  List activeColor = [true, false, false, false];
 
   @override
   void initState() {
@@ -34,6 +57,17 @@ class _HomePageState extends State<HomePage> {
         print("not logged in");
       }
     });
+    for (int i = 0; i < 7; i++) {
+      final date = _currentDate.add(Duration(days: i));
+      if (DateFormat("EEEE").format(date) != 'Saturday' &&
+          DateFormat("EEEE").format(date) != 'Sunday') {
+        dates.add(
+          _dayFormatter.format(date),
+          // _monthFormatter.format(date),
+        );
+      }
+    }
+    dayG = dates[0];
   }
 
   saveUid() async {
@@ -89,11 +123,7 @@ class _HomePageState extends State<HomePage> {
         String day = _dayFormatter
             .format(_currentDate.add(Duration(days: 3)))
             .toString();
-         databaseReference
-            .child("operators")
-            .child(uid)
-            .child("slots")
-            .set({
+        databaseReference.child("operators").child(uid).child("slots").set({
           day: {
             "10_11": false,
             "11_12": false,
@@ -108,20 +138,37 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  getData(String day) async {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User user = await auth.currentUser!;
+    final uid = user.uid;
+    final databaseReference = FirebaseDatabase.instance.ref();
+    DatabaseEvent event = await databaseReference.once();
+    Map<dynamic, dynamic> databaseData = event.snapshot.value as Map;
+    if (databaseData['operators'] != null) {
+      Map<dynamic, dynamic> slotData = databaseData['operators'][uid]['slots'];
+      print('SLOT ==== $slot');
+      print('DAY - $day');
+      dynamic keys_list = slotData.keys.toList();
+
+      status.clear();
+      status.addAll([
+        slotData[day]['10_11'],
+        slotData[day]['11_12'],
+        slotData[day]['12_1'],
+        false,
+        slotData[day]['2_3'],
+        slotData[day]['3_4'],
+        slotData[day]['4_5'],
+        slotData[day]['5_6']
+      ]);
+
+      return status;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    List dates = [];
-    for (int i = 0; i < 7; i++) {
-      final date = _currentDate.add(Duration(days: i));
-      if (DateFormat("EEEE").format(date) != 'Saturday' &&
-          DateFormat("EEEE").format(date) != 'Sunday') {
-        dates.add(
-          _dayFormatter.format(date),
-          // _monthFormatter.format(date),
-        );
-      }
-    }
-
     return Scaffold(
       drawer: NavigationDrawer(),
       appBar: AppBar(
@@ -186,39 +233,71 @@ class _HomePageState extends State<HomePage> {
               padding: const EdgeInsets.all(10.0),
               child: Row(
                 children: [
-                  Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        dates[0],
-                        style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        dayG = dates[0];
+                        activeColor = [false, false, false, true];
+                      });
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          dates[0],
+                          style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                        ),
                       ),
                     ),
                   ),
-                  Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        dates[1],
-                        style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        dayG = dates[1];
+                        activeColor = [false, false, false, true];
+                      });
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          dates[1],
+                          style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                        ),
                       ),
                     ),
                   ),
-                  Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        dates[2],
-                        style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        dayG = dates[2];
+                        activeColor = [false, false, false, true];
+                      });
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          dates[2],
+                          style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                        ),
                       ),
                     ),
                   ),
-                  Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        dates[3],
-                        style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        dayG = dates[3];
+                        activeColor = [false, false, false, true];
+                      });
+                    },
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          dates[3],
+                          style: TextStyle(fontFamily: 'Poppins', fontSize: 14),
+                        ),
                       ),
                     ),
                   ),
@@ -228,64 +307,103 @@ class _HomePageState extends State<HomePage> {
           ),
           SingleChildScrollView(
             scrollDirection: Axis.vertical,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: [
-                  Card(
-                    child: ListTile(
-                      title: Text('10AM to 11AM'),
-                      trailing: ElevatedButton(
-                        child: Text('Details'),
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          backgroundColor:
-                              MaterialStateProperty.all(Color(0xFFF23F44)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      tileColor: Color(0xffffffff),
-                      leading: Icon(
-                        Icons.circle,
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                  Card(
-                    child: ListTile(
-                      title: Text('11AM to 12AM'),
-                      trailing: ElevatedButton(
-                        child: Text('Vacant'),
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.black),
-                          backgroundColor:
-                              MaterialStateProperty.all(Color(0xffffffff)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(24.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      tileColor: Color(0xffffffff),
-                      leading: Icon(
-                        Icons.circle,
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            child: Column(
+              children: [
+                FutureBuilder(
+                    future: getData(dayG.toString()),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.waiting:
+                          return Padding(
+                              padding: EdgeInsets.only(top: 300),
+                              child: CupertinoActivityIndicator());
+                        case ConnectionState.none:
+                          return Text('none');
+                        case ConnectionState.active:
+                          return Text('active');
+                        case ConnectionState.done:
+                          return ListView.builder(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.vertical,
+                              itemCount: status.length,
+                              itemBuilder: (context, i) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(5.0),
+                                  child: Column(
+                                    children: [
+                                      timings[i] == '1PM - 2PM'
+                                          ? Card(
+                                              child: ListTile(
+                                                  title: Center(
+                                                child: Text(
+                                                  'LUNCH BREAK',
+                                                  style: TextStyle(
+                                                      fontFamily: 'Poppins',
+                                                      fontSize: 14),
+                                                ),
+                                              )),
+                                            )
+                                          : Card(
+                                              child: ListTile(
+                                                title: Text(timings[i]),
+                                                trailing: ElevatedButton(
+                                                  child: snapshot.data[i]
+                                                      ? Text(
+                                                          'Details',
+                                                          style: TextStyle(
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontSize: 14),
+                                                        )
+                                                      : Text(
+                                                          'Vacant',
+                                                          style: TextStyle(
+                                                              fontFamily:
+                                                                  'Poppins',
+                                                              fontSize: 14),
+                                                        ),
+                                                  onPressed: () {},
+                                                  style: ButtonStyle(
+                                                    foregroundColor:
+                                                        MaterialStateProperty
+                                                            .all<Color>(
+                                                                Colors.white),
+                                                    backgroundColor: snapshot
+                                                            .data[i]
+                                                        ? MaterialStateProperty
+                                                            .all(Color(
+                                                                0xFFF23F44))
+                                                        : MaterialStateProperty
+                                                            .all(Colors
+                                                                .grey.shade300),
+                                                    shape: MaterialStateProperty
+                                                        .all<
+                                                            RoundedRectangleBorder>(
+                                                      RoundedRectangleBorder(
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(24.0),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                                tileColor: Color(0xffffffff),
+                                                leading: Icon(
+                                                  Icons.circle,
+                                                  color: snapshot.data[i]
+                                                      ? Colors.red
+                                                      : Colors.green,
+                                                ),
+                                              ),
+                                            ),
+                                    ],
+                                  ),
+                                );
+                                ;
+                              });
+                      }
+                    })
+              ],
             ),
           ),
 
