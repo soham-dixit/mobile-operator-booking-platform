ab3f33c3addf73b04dd88941723195d53b9d9d7d
sohamdixit <sohamddixit@gmail.com>
2022-08-21 16:07:42 +0530

    future builder operator

--- aapka_aadhaar/lib/pages/previous_bookings.dart
+++ aapka_aadhaar/lib/pages/previous_bookings.dart
@@ -3,10 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class PreviousBookings extends StatefulWidget {
   const PreviousBookings({Key? key}) : super(key: key);
